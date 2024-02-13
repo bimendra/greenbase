f73367792f34b17ac1d834c57fbd9687ef835f3d
Bimendra Bandara <bimendra.c.bandara@gmail.com>
2024-02-13 09:31:21 +0800

    feat: Implement custom icons with ng-icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIconComponent } from '@ng-icons/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIconComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'greenbase';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { provideNgIconLoader, provideNgIconsConfig } from '@ng-icons/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), [importProvidersFrom(HttpClientModule)]],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+    provideNgIconLoader((name) => {
+      const http = inject(HttpClient);
+      return http.get(`/assets/icons/${name}.svg`, { responseType: 'text' });
+    }),
+    provideNgIconsConfig({
+      size: '24px',
+    }),
+  ],
 };
